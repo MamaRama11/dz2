934c6cded53220caaac6eedbe40b84df377fa35e
Shizz <shizzz777@yandex.ru>
2023-08-15 12:21:37 +0300

    29

--- tasl2/Program.cs
+++ tasl2/Program.cs
@@ -1,4 +1,4 @@
-﻿﻿Console.WriteLine("Введите три числа, и узнайте, ");
+﻿Console.WriteLine("Введите три числа, и узнайте, ");
 Console.WriteLine("какое из них имеет максимальное значение.");
 Console.Write("Введите первое число и нажмите ENTER: ");
 int num1 = int.Parse(Console.ReadLine());
